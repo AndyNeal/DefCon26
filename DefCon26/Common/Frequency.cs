69fbd46c999553750bf2bfa94e8334650dc200f1
Andy Neal <aneal@hosting.com>
2018-08-07 13:14:45 -0600

    more junk

---
+++ DefCon26/Common/Frequency.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public class Frequency
+    {
+        public static List<CharCount> CharsFromString(string str)
+        {
+            var result = new List<CharCount>();
+            var chars = str.ToCharArray();
+            foreach (char chr in chars)
+            {
+                bool found = false;
+                foreach(var pair in result)
+                {
+                    if (pair.Character == chr)
+                    {
+                        pair.Count++;
+                        found = true;
+                    }
+                }
+                if (!found)
+                {
+                    var item = new CharCount() { Character = chr, Count = 1 };
+                    result.Add(item);
+                }
+            }
+
+            var orderedResult = result.OrderByDescending(x => x.Count).ToList();
+
+            return orderedResult;
+        }
+
+        public static List<LocationData> LocationsInString(string str, char[] chars)
+        {
+            var chrs = str.ToCharArray();
+            var result = new List<LocationData>();
+
+            int i = 0;
+            foreach (char chr in chrs)
+            {
+                if (chars.Contains(chr))
+                {
+                    if (result.Any(l => l.Character == chr))
+                    {
+                        result.Single(l => l.Character == chr).Locations.Add(i);
+                    }
+                    else
+                    {
+                        result.Add(new LocationData() { Character = chr, Locations = new List<int>() { i } });
+                    }
+                }
+                i++;
+            }
+
+            return result;
+        }
+
+        public static string DropFromString(string str, char[] chrs, char replace = ' ')
+        {
+            var chars = str.ToCharArray();
+            string result = "";
+            foreach (char chr in chars)
+            {
+                if (chrs.Contains(chr))
+                {
+                    result += replace;
+                }
+                else
+                {
+                    result += chr;
+                }
+            }
+            return result;
+        }
+
+        public static string DropAllExceptFromString(string str, char[] chrs, char replace = ' ')
+        {
+            var chars = str.ToCharArray();
+            string result = "";
+            foreach (char chr in chars)
+            {
+                if (chrs.Contains(chr))
+                {
+                    result += chr;
+                }
+                else
+                {
+                    result += replace;
+                }
+            }
+            return result;
+        }
+
+    }
+
+    public class CharCount
+    {
+        public char Character;
+        public int Count;
+    }
+
+    public class LocationData
+    {
+        public char Character;
+        public List<int> Locations;
+    }
+
+
+}
--- DefCon26/TranspositionCipher/Program.cs
+++ DefCon26/TranspositionCipher/Program.cs
@@ -4,6 +4,10 @@ using System.IO;
 using System.Linq;
 using Common;
 
+//http://tholman.com/other/transposition/
+//https://www.dcode.fr/transposition-cipher
+
+
 namespace TranspositionCipher
 {
     class Program
@@ -11,6 +15,27 @@ namespace TranspositionCipher
         static void Main(string[] args)
         {
             string enc = "hr/m8ct.erfomn/aetwd/uce8clspwdmt/ndhp/swiouatxa_:/tcrms8ne/.a/g";
+            //           "h /   t       /  t  /      sp   t/  hp/s     t   :/t   s   /  / "
+            //string sanitized = Frequency.DropAllExceptFromString(enc, new char[] { 'h', 't', 'p', 's', ':', '/' }, ' ');
+            //Console.WriteLine("\"" + sanitized + "\"");
+            //Clipboard.SetText(sanitized);
+
+            var counts = Frequency.CharsFromString(enc);
+            foreach (var item in counts)
+            {
+                Console.WriteLine(item.Character + " : " + item.Count);
+            }
+
+            //var locations = Frequency.LocationsInString(enc, new char[] { 'h', 't', 'p', 's', ':', '/' });
+            //foreach (var loc in locations)
+            //{
+            //    Console.Write(loc.Character + " :");
+            //    foreach (var l in loc.Locations)
+            //    {
+            //        Console.Write(" " + l + ",");
+            //    }
+            //    Console.WriteLine();
+            //}
 
 
             //int x = 31;
@@ -48,27 +73,28 @@ namespace TranspositionCipher
             //}
 
 
-            int i = 0;
-            List<List<string>> grid = new List<List<string>>();
+            //int i = 0;
+            //List<List<string>> grid = new List<List<string>>();
 
-            for (int j = 0; j < 8; j++)
-            {
-                var temp = new List<string>();
-                for (int k = 0; k < 8; k++)
-                {
-                    temp.Add("+");
-                }
-                grid.Add(temp);
-            }
+            //for (int j = 0; j < 8; j++)
+            //{
+            //    var temp = new List<string>();
+            //    for (int k = 0; k < 8; k++)
+            //    {
+            //        temp.Add("+");
+            //    }
+            //    grid.Add(temp);
+            //}
 
-            for (int j = 0; j < 8; j++)
-            {
-                for (int k = 0; k < 8; k++)
-                {
-                    grid[k][j] = enc.Substring(i, 1);
-                    i++;
-                }
-            }
+            //for (int j = 0; j < 8; j++)
+            //{
+            //    for (int k = 0; k < 8; k++)
+            //    {
+            //        //grid[k][j] = enc.Substring(i, 1);
+            //        grid[k][j] = sanitized.Substring(i, 1);
+            //        i++;
+            //    }
+            //}
 
             //for (int j = 0; j < 8; j++)
             //{
@@ -79,38 +105,38 @@ namespace TranspositionCipher
             //    Console.WriteLine();
             //}
 
-            var bar = Permutations.GeneratePermutations(grid);
-            StreamWriter sw = new StreamWriter("output.txt");
-            List<string> stuff = new List<string>();
-            foreach (var item in bar)
-            {
-                //for (int j = 0; j < 8; j++)
-                //{
-                //    for (int k = 0; k < 8; k++)
-                //    {
-                //        Console.Write(item[k][j]);
-                //        sw.Write(item[k][j]);
-                //    }
-                //    Console.WriteLine();
-                //    sw.WriteLine();
-                //}
-                string x = item[0][0] + item[1][1] + item[2][2] + item[3][3] + item[4][4] + item[5][5] + item[6][6] + item[7][7];
-                if (!stuff.Contains(x))
-                {
-                    stuff.Add(x);
-                }
-                //Console.WriteLine();
-                //sw.WriteLine();
-            }
+            //var bar = Permutations.GeneratePermutations(grid);
+            //StreamWriter sw = new StreamWriter("output.txt");
+            //List<string> stuff = new List<string>();
+            //foreach (var item in bar)
+            //{
+            //    //for (int j = 0; j < 8; j++)
+            //    //{
+            //    //    for (int k = 0; k < 8; k++)
+            //    //    {
+            //    //        Console.Write(item[k][j]);
+            //    //        sw.Write(item[k][j]);
+            //    //    }
+            //    //    Console.WriteLine();
+            //    //    sw.WriteLine();
+            //    //}
+            //    string x = item[0][0] + item[1][1] + item[2][2] + item[3][3] + item[4][4] + item[5][5] + item[6][6] + item[7][7];
+            //    if (!stuff.Contains(x))
+            //    {
+            //        stuff.Add(x);
+            //    }
+            //    //Console.WriteLine();
+            //    //sw.WriteLine();
+            //}
 
-            foreach (var item in stuff)
-            {
-                sw.WriteLine(item);
-            }
+            //foreach (var item in stuff)
+            //{
+            //    sw.WriteLine(item);
+            //}
 
-            sw.Close();
+            //sw.Close();
 
-            //var key = Console.ReadKey();
+            var key = Console.ReadKey();
         }
     }
 }
